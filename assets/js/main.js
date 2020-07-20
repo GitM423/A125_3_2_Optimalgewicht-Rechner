0cac5489173f52a0adcc6f495a5e8111d808740c
GitM423 <matthias.p.hoeh@gmail.com>
2020-07-20 11:37:08 +0200

    v0.0.1

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+function checkBuild() {
+    if (document.getElementById("wide").checked == true) {
+        console.log('selected: \n\t"Breite"')
+    }
+    else if (document.getElementById("slim").checked == true) {
+        console.log('selected: \n\t"Schmale"')
+    }
+}
+
+function sendData() {
+    let height = document.getElementById("height").value
+    let age = document.getElementById("age").value
+    let idealWeight
+
+    if (height == "" || age == "") {
+        console.log("ERROR: \n\theight and/or age is missing")
+        document.getElementById("res").innerHTML = "ERROR: height and/or age is missing"
+    }
+    else {
+        if (document.getElementById("wide").checked == true) {
+            build = "wide"
+            idealWeight = ((height - 100) + (age / 10)) * 0.9 * 1.1
+        }
+        else if (document.getElementById("slim").checked == true) {
+            build = "slim"
+            idealWeight = ((height - 100) + (age / 10)) * 0.9 * 0.9
+        }
+
+        console.log("height: \n\t" + height + " cm")
+        console.log("age: \n\t" + age + " years")
+        console.log("build: \n\t" + build)
+        console.log("idealWeight: \n\t" + idealWeight.toFixed(2) + " kg")
+
+        document.getElementById("res").innerHTML = `Dein Idealgewicht ist ${idealWeight.toFixed(2).split(".").join(",")} kg.`
+    }
+}
+
+document.querySelectorAll('input').forEach(function (item) {
+    item.addEventListener("keyup", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault()
+            document.getElementById("btn").click()
+        }
+    })
+})
